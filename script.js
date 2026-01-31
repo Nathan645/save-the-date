091df82daa89424a09773355d62e38fdab58350a
Nathan645 <56030184+Nathan645@users.noreply.github.com>
2026-02-01 08:39:18 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const eventDate = new Date("July 12, 2026 15:00:00").getTime();
+const countdown = document.getElementById("countdown");
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = eventDate - now;
+
+  if (distance < 0) {
+    countdown.innerHTML = "C'est le grand jour 🎉";
+    return;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  countdown.innerHTML = `${days} jours avant le jour J`;
+}, 1000);
